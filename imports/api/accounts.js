bcacd83be0eaa7395cfa097cd1dec555ad86e602
bomb-on <0nada0@gmail.com>
2024-07-28 13:43:46 +0000

    Cleanup

--- imports/api/accounts.js
+++ imports/api/accounts.js
@@ -1,15 +1,5 @@
 import { Accounts } from 'meteor/accounts-base';
 import { Meteor } from 'meteor/meteor';
-import { TestCollection } from './collection';
-
-
-Meteor.startup(async () => {
-  if ((await TestCollection.find().countAsync()) === 0) {
-    for (let i = 0; i < 10000; i++) {
-      await TestCollection.insertAsync({ name: `DOC ###${i}`, created: new Date(), updated: new Date() });
-    }
-  }
-});
 
 Accounts.createUserAsync = async options => {
   // Add additional data...
